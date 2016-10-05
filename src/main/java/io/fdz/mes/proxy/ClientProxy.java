f43ef96e8d54a36cf45e1bf8e703b9aabc4fecf7
FileDotZip <g.paremsky@gmail.com>
2016-10-05 09:33:57 -0400

    Finished Client Proxy(i hope)

--- src/main/java/io/fdz/mes/proxy/ClientProxy.java
+++ src/main/java/io/fdz/mes/proxy/ClientProxy.java
@@ -1,5 +1,23 @@
 package io.fdz.mes.proxy;
 
 
-public class ClientProxy {
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class ClientProxy extends CommonProxy{
+    @Override
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
+
+    @Override
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Override
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
 }
